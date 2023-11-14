74aa8b53a82ad1fb8428b27b763bca3040a0d073
Miguel Ángel Navas García <manavas@gmail.com>
2023-11-14 19:00:11 +0100

    First commit

---
+++ src/test/java/com/manavas/microservices/limitsservice/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manavas.microservices.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
