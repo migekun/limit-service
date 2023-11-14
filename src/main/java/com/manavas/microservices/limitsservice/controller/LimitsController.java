a713eb8176ac946b02f5cd221a81ea313fe44826
Miguel Ángel Navas García <manavas@gmail.com>
2023-11-14 19:11:33 +0100

    Added controller and bean limits to do the exercise

---
+++ src/main/java/com/manavas/microservices/limitsservice/controller/LimitsController.java
@@ -0,0 +1,14 @@
+package com.manavas.microservices.limitsservice.controller;
+
+import com.manavas.microservices.limitsservice.bean.Limits;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+
+    @GetMapping("/limits")
+    public Limits retrieveLimits(){
+        return new Limits(1, 1000);
+    }
+}
